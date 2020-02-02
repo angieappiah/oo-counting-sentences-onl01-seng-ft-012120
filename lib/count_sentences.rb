83dcc9ad26272cdf2d308b536dfa17c4164757df
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-02 22:18:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -10,7 +10,7 @@ class String
     return false
   end
 end
-    
+  
 
   def question?
     if
@@ -37,7 +37,5 @@ end
     new_array = self.split(/[.?!]/).reject {|string| string.empty?}
     return new_array.length
   end
-    
-
   end
 end
